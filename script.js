ba8b9fe8caca3cf4376bb1508246169c46a2cabd
Kimori <william.pellas@hotmail.com>
2022-12-20 12:33:29 +0100

    1.3

--- script.js
+++ script.js
@@ -32,6 +32,23 @@ function calculateReset() {
     num1.value = "",
     num2.value = ""
 }
+function validate(evt) {
+    var theEvent = evt || window.event;
+  
+    // Handle paste
+    if (theEvent.type === 'paste') {
+        key = event.clipboardData.getData('text/plain');
+    } else {
+    // Handle key press
+        let key = theEvent.keyCode || theEvent.which;
+        key = String.fromCharCode(key);
+    }
+    let regex = /[0-9]|\./;
+    if( !regex.test(key) ) {
+      theEvent.returnValue = false;
+      if(theEvent.preventDefault) theEvent.preventDefault();
+    }
+  }
 
 document.getElementById("calcBtnAdd").addEventListener('click', calculatePlus);
 document.getElementById("calcBtnMin").addEventListener('click', calculateMinus);
