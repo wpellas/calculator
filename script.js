3d50e693b5eda0651f40cae5a81682b999471f13
Kimori <william.pellas@hotmail.com>
2022-12-20 12:35:50 +0100

    1.3.1

--- script.js
+++ script.js
@@ -40,10 +40,10 @@ function validate(evt) {
         key = event.clipboardData.getData('text/plain');
     } else {
     // Handle key press
-        let key = theEvent.keyCode || theEvent.which;
+        var key = theEvent.keyCode || theEvent.which;
         key = String.fromCharCode(key);
     }
-    let regex = /[0-9]|\./;
+    var regex = /[0-9]|\./;
     if( !regex.test(key) ) {
       theEvent.returnValue = false;
       if(theEvent.preventDefault) theEvent.preventDefault();
