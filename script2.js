5c5f9067bb780b184bf9afcdc2ba404c34f14bb2
Kimori <william.pellas@hotmail.com>
2022-12-21 22:04:21 +0100

    1.4.1

---
+++ script2.js
@@ -0,0 +1,8 @@
+function alexNamn() {
+    let name = prompt("What is your name?");
+    if (name === "Alex") {
+        alert("Hej, Alex!")
+    } else {
+        alert("Vem fan är du?")
+    }
+};
