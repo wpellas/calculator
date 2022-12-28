9438068e87b35578452af899b502c2ac248aae35
william <william.pellas@hotmail.com>
2022-12-28 10:50:10 +0100

    2.0

--- script.js
+++ script.js
@@ -1,40 +1,31 @@
-let num1 = document.getElementById("num1");
-let num2 = document.getElementById("num2");
-let result = document.getElementById("result");
+let display = document.getElementById("display"); // Gives us access to the display div
 
-// function errorAlert() {
-//     if (result.value = "") {
-//     alert("Please enter a value in both boxes.");
-//     }
-// }
+// Converts HTMLCollection into an array.
+let buttons = Array.from(document.getElementsByClassName("button"));
 
-// advanced function
-// let calcPlus = () => {result.value = +num1 + +num2;};
-
-function calculatePlus() {
-     result.value =
-     +num1.value + +num2.value;
-}
-function calculateMinus() {
-    result.value =
-    +num1.value - +num2.value;
-}
-function calculateMultiply() {
-    result.value =
-    +num1.value * +num2.value;
-}
-function calculateDivide() {
-    result.value =
-    +num1.value / +num2.value;
-}
-function calculateReset() {
-    result.value = "",
-    num1.value = "",
-    num2.value = ""
-}
-
-document.getElementById("calcBtnAdd").addEventListener('click', calculatePlus);
-document.getElementById("calcBtnMin").addEventListener('click', calculateMinus);
-document.getElementById("calcBtnMul").addEventListener('click', calculateMultiply);
-document.getElementById("calcBtnDiv").addEventListener('click', calculateDivide);
-document.getElementById("calcBtnReset").addEventListener('click', calculateReset);
+buttons.map(button => {
+    button.addEventListener('click', (eVent) => {
+        switch(eVent.target.innerText){ // Looks for the text within the current target, so the text inside the html element of the button
+            case 'C': // If C is pressed
+                display.innerText = ''; // Turns Display into an empty string (clears it)
+                break;
+            case '←': // if backarrow is pressed
+                if(display.innerText){ // Checks if something exists within the display string
+                    display.innerText = display.innerText.slice(0, -1); // Removes the latest item in the string
+                }
+                break;
+            case '=': // if equals is pressed
+                try {
+                    display.innerText = eval(display.innerText); // Calculates everything inputted in the string in mathematical order
+                } catch {
+                    display.innerText = "Error!"; // Returns an errortext if the inputs are wrong
+                }
+                break;
+            // case ".": // If you don't want more than one dot, but we do need the ability to use more than one.
+            //     if(display.innerText = ".")
+            //     break;
+            default:
+                display.innerText += eVent.target.innerText; // Adds the target button text content as a string to the display
+        }
+    });
+});
